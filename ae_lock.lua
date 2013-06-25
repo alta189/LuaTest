e9e6380e637cf608bdfc145018261dc251fbf1bc
Stephen Williams <me@alta189.com>
2013-06-24 22:32:27 -0300

    Update ae_lock.lua

--- ae_lock.lua
+++ ae_lock.lua
@@ -2,12 +2,16 @@ os.loadAPI("ocs/apis/sensor")
 local prox = sensor.wrap("left")
 
 term.redirect(peripheral.wrap("right"))
+local x = 0
+local y = 0
 
 while (true) do
   local targets = prox.getTargets()
   for name, details in pairs(targets) do
     print("Detected: " ..name)
-    print("Position: " textutils.serialize(details["Position"]))
+    if (math.floor(details["Position"]["Z"]) == -1 and (math.floor(details["Position"]["X"]) ~= x or math.floor(details["Position"]["Y"]))) then
+      print("X: " .. math.floor(details["Position"]["x"]) .. ", Y: " .. math.floor(details["Position"]["Y"]))
+    end
 end
 
 term.restore()
