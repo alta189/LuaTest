b1a1b7613d795a9842e0fc0cade54a369f873e63
Stephen Williams <me@alta189.com>
2013-06-24 23:13:42 -0300

    Update ae_lock.lua

--- ae_lock.lua
+++ ae_lock.lua
@@ -5,12 +5,19 @@ term.redirect(peripheral.wrap("right"))
  
 while (true) do
   local targets = prox.getTargets()
+  local found = false
   for name, details in pairs(targets) do
-    if (math.floor(details["Position"]["Y"]) == -1 and redstone.getInput("front")) then
+    if (math.floor(details["Position"]["Y"]) == -1) then
       print("Detected: " ..name)
-      print("X: " .. math.floor(details["Position"]["X"]) .. ", Z: " .. math.floor(details["Position"]["Z"]))
+      --print("X: " .. math.floor(details["Position"]["X"]) .. ", Z: " .. math.floor(details["Position"]["Z"]))
+      local x = math.floor(details["Position"]["X"])
+      local z =  math.floor(details["Position"]["Z"])
+      if ((x >= -3 and x <= 5) and (z >= -7 and x <= 4)) then
+         found = true
+      end
     end
-   end
+  end
+  redstone.setOutput("back", found)
 end
  
 term.restore()
