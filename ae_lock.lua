7408c2ac0d57f1c91aa24c6a72c29dd69eb772cd
Stephen Williams <me@alta189.com>
2013-06-24 21:38:22 -0400

    Update ae_lock.lua

--- ae_lock.lua
+++ ae_lock.lua
@@ -8,8 +8,9 @@ local y = 0
 while (true) do
   local targets = prox.getTargets()
   for name, details in pairs(targets) do
-    print("Detected: " ..name)
+    print(math.floor(details["Position"]["Z"]))
     if (math.floor(details["Position"]["Z"]) == -1 and (math.floor(details["Position"]["X"]) ~= x or math.floor(details["Position"]["Y"]))) then
+      print("Detected: " ..name)
       print("X: " .. math.floor(details["Position"]["x"]) .. ", Y: " .. math.floor(details["Position"]["Y"]))
     end
    end
