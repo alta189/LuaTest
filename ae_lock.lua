136a7b6888e80849e633a1bfff94808ccf76a7c8
Stephen Williams <me@alta189.com>
2013-06-24 22:12:16 -0300

    Update ae_lock.lua

--- ae_lock.lua
+++ ae_lock.lua
@@ -1,5 +1,6 @@
 os.loadAPI("ocs/apis/sensor")
 local prox = sensor.wrap("left")
+os.sleep(10)
 local targets = prox.getTargets()
 for name, details in pairs(targets) do
   print("Raining: " ..details["Name"])
