40a24e6ccf54fa91302936728d92d8914bb748a2
Stephen Williams <me@alta189.com>
2013-06-24 21:34:29 -0400

    Update ae_lock.lua

--- ae_lock.lua
+++ ae_lock.lua
@@ -12,6 +12,7 @@ while (true) do
     if (math.floor(details["Position"]["Z"]) == -1 and (math.floor(details["Position"]["X"]) ~= x or math.floor(details["Position"]["Y"]))) then
       print("X: " .. math.floor(details["Position"]["x"]) .. ", Y: " .. math.floor(details["Position"]["Y"]))
     end
+   end
 end
 
 term.restore()
