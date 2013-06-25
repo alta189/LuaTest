103e96ccac2b36f80e156476ce3cace24bc1d5e3
Stephen Williams <me@alta189.com>
2013-06-24 22:17:33 -0400

    Update ae_lock.lua

--- ae_lock.lua
+++ ae_lock.lua
@@ -1,15 +1,12 @@
 os.loadAPI("ocs/apis/sensor")
 local prox = sensor.wrap("left")
  
-term.redirect(peripheral.wrap("right"))
- 
 while (true) do
   local targets = prox.getTargets()
   local found = false
   for name, details in pairs(targets) do
     if (math.floor(details["Position"]["Y"]) == -1) then
       print("Detected: " ..name)
-      --print("X: " .. math.floor(details["Position"]["X"]) .. ", Z: " .. math.floor(details["Position"]["Z"]))
       local x = math.floor(details["Position"]["X"])
       local z =  math.floor(details["Position"]["Z"])
       if ((x >= -3 and x <= 7) and (z >= -7 and x <= 4)) then
@@ -18,6 +15,5 @@ while (true) do
     end
   end
   redstone.setOutput("back", not found)
+  os.sleep(.5)
 end
- 
-term.restore()
